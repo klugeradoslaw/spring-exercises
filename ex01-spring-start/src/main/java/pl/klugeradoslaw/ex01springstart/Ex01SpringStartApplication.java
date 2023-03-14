fba8b7b1f87b7420559f4f28c645a53a37224da1
Radoslaw Kluge <kluge.radoslaw@gmail.com>
2023-03-14 09:52:22 +0100

    ex01 added to repo

---
+++ ex01-spring-start/src/main/java/pl/klugeradoslaw/ex01springstart/Ex01SpringStartApplication.java
@@ -0,0 +1,13 @@
+package pl.klugeradoslaw.ex01springstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex01SpringStartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ex01SpringStartApplication.class, args);
+    }
+
+}
---
+++ ex01-spring-start/src/test/java/pl/klugeradoslaw/ex01springstart/Ex01SpringStartApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.klugeradoslaw.ex01springstart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ex01SpringStartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
