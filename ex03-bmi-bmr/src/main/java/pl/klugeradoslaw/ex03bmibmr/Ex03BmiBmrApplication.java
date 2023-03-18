6d0b5f5bc70e9ca66bdadf9fc7dd5f1b4634e6a3
Radoslaw Kluge <kluge.radoslaw@gmail.com>
2023-03-18 19:50:33 +0100

    added methods to calculate BMI and BMR in Calculator claass (service)

--- ex03-bmi-bmr/src/main/java/pl/klugeradoslaw/ex03bmibmr/Ex03BmiBmrApplication.java
+++ ex03-bmi-bmr/src/main/java/pl/klugeradoslaw/ex03bmibmr/Ex03BmiBmrApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Ex03BmiBmrApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Ex03BmiBmrApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Ex03BmiBmrApplication.class, args);
+    }
 
 }
--- ex03-bmi-bmr/src/test/java/pl/klugeradoslaw/ex03bmibmr/Ex03BmiBmrApplicationTests.java
+++ ex03-bmi-bmr/src/test/java/pl/klugeradoslaw/ex03bmibmr/Ex03BmiBmrApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class Ex03BmiBmrApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
