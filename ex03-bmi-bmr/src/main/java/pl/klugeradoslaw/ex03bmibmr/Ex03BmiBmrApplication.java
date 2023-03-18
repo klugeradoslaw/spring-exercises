650945af6e09c226bd4d6355dbc9ef038cff4985
Radoslaw Kluge <kluge.radoslaw@gmail.com>
2023-03-18 19:36:15 +0100

    new exercises03

---
+++ ex03-bmi-bmr/src/main/java/pl/klugeradoslaw/ex03bmibmr/Ex03BmiBmrApplication.java
@@ -0,0 +1,13 @@
+package pl.klugeradoslaw.ex03bmibmr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex03BmiBmrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ex03BmiBmrApplication.class, args);
+	}
+
+}
---
+++ ex03-bmi-bmr/src/test/java/pl/klugeradoslaw/ex03bmibmr/Ex03BmiBmrApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.klugeradoslaw.ex03bmibmr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ex03BmiBmrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
