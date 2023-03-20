53d510358a510b026bd49722a8aabea98e2a190e
Radoslaw Kluge <kluge.radoslaw@gmail.com>
2023-03-20 18:05:57 +0100

    ex04 - student management system - first commit

---
+++ ex04-student-management-system/src/main/java/pl/klugeradoslaw/ex04studentmanagementsystem/Ex04StudentManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package pl.klugeradoslaw.ex04studentmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex04StudentManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ex04StudentManagementSystemApplication.class, args);
+    }
+
+}
---
+++ ex04-student-management-system/src/test/java/pl/klugeradoslaw/ex04studentmanagementsystem/Ex04StudentManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.klugeradoslaw.ex04studentmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ex04StudentManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
