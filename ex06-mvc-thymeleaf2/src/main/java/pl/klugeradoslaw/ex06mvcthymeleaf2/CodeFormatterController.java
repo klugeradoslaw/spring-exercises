e40e1f1ad95a12c4b61de1b8bb42d86030b04e88
klugeradoslaw <kluge.radoslaw@gmail.com>
2023-03-21 15:32:02 +0100

    ex06 added to repo

---
+++ ex06-mvc-thymeleaf2/src/main/java/pl/klugeradoslaw/ex06mvcthymeleaf2/CodeFormatterController.java
@@ -0,0 +1,33 @@
+package pl.klugeradoslaw.ex06mvcthymeleaf2;
+
+import com.google.googlejavaformat.java.FormatterException;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+public class CodeFormatterController {
+    private final CodeFormatterService codeFormatterService;
+
+    public CodeFormatterController(CodeFormatterService codeFormatterService) {
+        this.codeFormatterService = codeFormatterService;
+    }
+
+    @GetMapping("/")
+    String home() {
+        return "index";
+    }
+
+    @PostMapping("/format")
+    String formatCode(@RequestParam String code, Model model) {
+        try {
+            String formattedCode = codeFormatterService.formatCode(code);
+            model.addAttribute("formattedCode", formattedCode);
+        } catch (FormatterException e) {
+            return "wrong-code";
+        }
+        model.addAttribute("unformattedCode", code);
+        return "result-code";
+    }
+
+}
---
+++ ex06-mvc-thymeleaf2/src/test/java/pl/klugeradoslaw/ex06mvcthymeleaf2/Ex06MvcThymeleaf2ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.klugeradoslaw.ex06mvcthymeleaf2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ex06MvcThymeleaf2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
