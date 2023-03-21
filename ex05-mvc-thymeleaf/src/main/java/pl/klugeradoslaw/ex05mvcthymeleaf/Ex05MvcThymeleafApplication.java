39cb43296e880732a189e60a912e128504e6fd0f
klugeradoslaw <kluge.radoslaw@gmail.com>
2023-03-21 11:46:54 +0100

    ex05 added to repo

---
+++ ex05-mvc-thymeleaf/src/main/java/pl/klugeradoslaw/ex05mvcthymeleaf/Ex05MvcThymeleafApplication.java
@@ -0,0 +1,13 @@
+package pl.klugeradoslaw.ex05mvcthymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex05MvcThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ex05MvcThymeleafApplication.class, args);
+	}
+
+}
---
+++ ex05-mvc-thymeleaf/src/test/java/pl/klugeradoslaw/ex05mvcthymeleaf/Ex05MvcThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.klugeradoslaw.ex05mvcthymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ex05MvcThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
