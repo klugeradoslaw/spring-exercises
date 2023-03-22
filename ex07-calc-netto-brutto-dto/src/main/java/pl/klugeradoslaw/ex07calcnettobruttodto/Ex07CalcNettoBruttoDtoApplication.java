1274c1cf17af9304b8e39204ff0d3f11be444275
Radoslaw Kluge <kluge.radoslaw@gmail.com>
2023-03-22 19:35:06 +0100

    ex07 started

---
+++ ex07-calc-netto-brutto-dto/src/main/java/pl/klugeradoslaw/ex07calcnettobruttodto/Ex07CalcNettoBruttoDtoApplication.java
@@ -0,0 +1,13 @@
+package pl.klugeradoslaw.ex07calcnettobruttodto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex07CalcNettoBruttoDtoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ex07CalcNettoBruttoDtoApplication.class, args);
+    }
+
+}
---
+++ ex07-calc-netto-brutto-dto/src/test/java/pl/klugeradoslaw/ex07calcnettobruttodto/Ex07CalcNettoBruttoDtoApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.klugeradoslaw.ex07calcnettobruttodto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ex07CalcNettoBruttoDtoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
