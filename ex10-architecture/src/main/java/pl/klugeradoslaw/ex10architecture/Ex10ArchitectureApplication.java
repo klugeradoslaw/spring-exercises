347465826543f82e7f18d726574ff29f3bb081b0
Radoslaw Kluge <kluge.radoslaw@gmail.com>
2023-03-31 22:20:55 +0200

    ex10 started before ex09

---
+++ ex10-architecture/src/main/java/pl/klugeradoslaw/ex10architecture/Ex10ArchitectureApplication.java
@@ -0,0 +1,13 @@
+package pl.klugeradoslaw.ex10architecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex10ArchitectureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ex10ArchitectureApplication.class, args);
+    }
+
+}
---
+++ ex10-architecture/src/test/java/pl/klugeradoslaw/ex10architecture/Ex10ArchitectureApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.klugeradoslaw.ex10architecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ex10ArchitectureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
