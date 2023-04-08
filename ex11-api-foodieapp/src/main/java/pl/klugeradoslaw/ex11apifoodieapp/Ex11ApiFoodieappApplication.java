0e4ef58d9e3e59e634f7f48b01dfa2855e433e54
Radoslaw Kluge <kluge.radoslaw@gmail.com>
2023-04-08 14:19:43 +0200

    ex11 started, foodie-app

--- ex11-api-foodieapp/src/main/java/pl/klugeradoslaw/ex11apifoodieapp/Ex11ApiFoodieappApplication.java
+++ ex11-api-foodieapp/src/main/java/pl/klugeradoslaw/ex11apifoodieapp/Ex11ApiFoodieappApplication.java
@@ -1,13 +1,13 @@
-package pl.klugeradoslaw.demo;
+package pl.klugeradoslaw.ex11apifoodieapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class Ex11ApiFoodieappApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        SpringApplication.run(Ex11ApiFoodieappApplication.class, args);
     }
 
 }
--- ex11-api-foodieapp/src/test/java/pl/klugeradoslaw/ex11apifoodieapp/Ex11ApiFoodieappApplicationTests.java
+++ ex11-api-foodieapp/src/test/java/pl/klugeradoslaw/ex11apifoodieapp/Ex11ApiFoodieappApplicationTests.java
@@ -1,10 +1,10 @@
-package pl.klugeradoslaw.demo;
+package pl.klugeradoslaw.ex11apifoodieapp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class Ex11ApiFoodieappApplicationTests {
 
     @Test
     void contextLoads() {
