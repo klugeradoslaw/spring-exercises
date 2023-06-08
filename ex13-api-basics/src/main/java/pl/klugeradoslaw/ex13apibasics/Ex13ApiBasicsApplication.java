94e740020e449570512361f73dd1d653b2c7e5ec
Radoslaw Kluge <kluge.radoslaw@gmail.com>
2023-06-08 17:03:08 +0200

    added Book entity class

---
+++ ex13-api-basics/src/main/java/pl/klugeradoslaw/ex13apibasics/Ex13ApiBasicsApplication.java
@@ -0,0 +1,13 @@
+package pl.klugeradoslaw.ex13apibasics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex13ApiBasicsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ex13ApiBasicsApplication.class, args);
+    }
+
+}
---
+++ ex13-api-basics/src/test/java/pl/klugeradoslaw/ex13apibasics/Ex13ApiBasicsApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.klugeradoslaw.ex13apibasics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ex13ApiBasicsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
