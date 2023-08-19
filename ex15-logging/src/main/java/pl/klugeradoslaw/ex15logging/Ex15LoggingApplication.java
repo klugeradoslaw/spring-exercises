db5379d6bc9060e384fbd058cf7b1423fcd25aab
Radoslaw Kluge <kluge.radoslaw@gmail.com>
2023-08-19 19:44:28 +0200

    comeback

---
+++ ex15-logging/src/main/java/pl/klugeradoslaw/ex15logging/Ex15LoggingApplication.java
@@ -0,0 +1,13 @@
+package pl.klugeradoslaw.ex15logging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex15LoggingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ex15LoggingApplication.class, args);
+    }
+
+}
---
+++ ex15-logging/src/test/java/pl/klugeradoslaw/ex15logging/Ex15LoggingApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.klugeradoslaw.ex15logging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ex15LoggingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
